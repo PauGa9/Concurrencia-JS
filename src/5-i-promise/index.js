742de593e42f042e26d861b0759891f17c948190
Pau Gasol <gasanpau@gmail.com>
2019-07-14 21:39:40 +0200

    fix: exercise 5

--- src/5-i-promise/index.js
+++ src/5-i-promise/index.js
@@ -1,22 +1,8 @@
-// const axios = require('axios')
-// const promiseFs = require('../services/promiseFs')
-// const filePath = `${__dirname}/../../test.txt`
-// const url = 'https://api.openbrewerydb.org/breweries?by_state=new_york'
+const axios = require('axios')
+const promiseFs = require('../services/promiseFs')
+const filePath = `${__dirname}/../../test.txt`
+const url = 'https://api.openbrewerydb.org/breweries?by_state=new_york'
 
- function aa() {
-    /** Añade código aquí */
-    var p1 = new Promise(function(resolve, reject){
-        resolve(1)
-    })
-    setTimeout(function(){
-      console.log("will be executed at the top of the next Event Loop")
-    },0)
-    p1.then(function(value){
-      console.log("p1 fulfilled")
-    })
-    setTimeout(function(){
-      console.log("will be executed at the bottom of the next Event Loop")
-    },0)
+module.exports.default = function() {
+  /** Añade código aquí */
 }
-
-aa()
