fcb9871906677be04ce377e27b4853dca79df5bc
Pau Gasol <gasanpau@gmail.com>
2019-07-15 18:23:47 +0200

    feat: add exercise 7 boilerplate and empty Readme

---
+++ src/7-order-buguer/index.js
@@ -0,0 +1,10 @@
+const order = require('../services/order')
+const orderMeat = order.orderMeat
+const orderBread = order.orderBread
+const orderCondiments = order.orderCondiments
+
+function makeBurguer() {
+    // Añade aquí tu código
+}
+
+module.exports.default = makeBurguer
