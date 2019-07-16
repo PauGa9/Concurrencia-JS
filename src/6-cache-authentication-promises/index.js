ed00da1a9ca50985f540a1caaa68566b1c1ebf4d
Pau Gasol <gasanpau@gmail.com>
2019-07-16 16:46:24 +0200

    refactor: add callback solution as boilerplate for exercise 6

--- src/6-cache-authentication-promises/index.js
+++ src/6-cache-authentication-promises/index.js
@@ -7,6 +7,32 @@ const authController = (request, response) => {
         token;
     
     /** Añade código aquí */
+
+    /* Aquí tienes una posible solución con callbacks
+    cache.get(userId, (error, token) => {
+        if (error) {
+            response.json({error: 'Uuups'})
+            return
+        }
+        if (token) {
+            response.json({token: token})
+            return
+        }
+        auth.authenticate(userId, password, (error, token) => {
+            if (error) {
+                response.json({error: 'Uuups'})
+                return
+            }
+            cache.set(userId, token, (error, insertions) => {
+                if (error) {
+                    response.json({error: 'Uuups'})
+                    return
+                }
+                response.json({token: '1234asdf-56fg-789hjk'})
+            })
+        })
+    })
+    */
 }
 
 module.exports.default = authController
